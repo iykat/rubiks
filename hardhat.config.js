5132d191c5bc802453520b9ae7168421d21465bd
Derek Addo <mr.derek.addo@gmail.com>
2023-10-26 10:12:17 +0000

    updated sendfunds function

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,17 +5,16 @@ require('dotenv').config();
 
 //dotenv.config (); 
 
-
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
   networks: {
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY]
     }
   }
